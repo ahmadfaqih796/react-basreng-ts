ef687d26f20a299317f221ae0bdfb07983edf360
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-01-16 10:00:49 +0700

    handle search params

--- app/routes/about.$aboutId.tsx
+++ app/routes/about.$aboutId.tsx
@@ -1,12 +1,29 @@
+import { useLocation, useSearchParams } from "@remix-run/react";
+
 export default function AboutRoute() {
-   return (
-     <div>
-       <p>Here's your hilarious joke:</p>
-       <p>
-         Why don't you find hippopotamuses hiding in trees?
-         They're really good at it.
-       </p>
-     </div>
-   );
- }
- 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { search } = useLocation();
+  const params = new URLSearchParams(search);
+  console.log("eeeeeeeeeeeee", params, searchParams);
+  console.log("333333", params.get("id"));
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>
+        Why don't you find hippopotamuses hiding in trees? They're really good
+        at it.
+      </p>
+      <button
+        onClick={() => {
+          params.set("someKey", "someValue");
+          params.set("id", "3232");
+          setSearchParams(params, {
+            preventScrollReset: true,
+          });
+        }}
+      >
+        Tombol
+      </button>
+    </div>
+  );
+}
